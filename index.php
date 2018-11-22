31ee59dfcb2518327d9fa33e4b7d282afbb1ec73
NadiSanli <43350588+NadiSanli@users.noreply.github.com>
2018-11-22 16:24:05 +0100

    Added index.php with Form

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: lukaskirchhoff
- * Date: 22.11.18
- * Time: 16:14
- */
+<html>
+<head>
+
+    <meta charset="UTF-8">
+    <title>SongOrganizer 2.0</title>
+</head>
+
+
+<body>
+
+<form action="overview.php" method="post">
+    <p><b>Add a New Song</b></p>
+    <p>Song Name: <input type="text" name="SongName"/></p>
+    <p>Artist: <input type="text" name="Artist"/></p>
+    <p>Playback ID: <input type="text" name="PlaybackID"/></p>
+
+
+    <p>
+        <input type="submit" name="submit"
+               value="Add Song to List"/>
+    </p>
+</form>
+
+
+</body>
+
+</html>
