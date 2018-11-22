e1298cbec42aa753adbafa643ebb0bd7e3b52dc9
Lukas Kirchhoff <lu.kirchhoff@googlemail.com>
2018-11-22 16:33:39 +0100

    added write to file

--- index.php
+++ index.php
@@ -8,7 +8,17 @@
 
 <body>
 
-<form action="overview.php" method="post">
+<?php
+// Save to file
+if (isset($_POST['submit'])){
+    $thingsToAdd = $_POST['SongName']  . "," . $_POST['Artist'] . "," . $_POST['PlaybackID'] . ";";
+    $file = fopen("songs.txt", "wb") or die("Unable to open file!");
+    fwrite($file, $thingsToAdd);
+    fclose($file);
+}
+?>
+
+<form action="index.php" method="POST">
     <p><b>Add a New Song</b></p>
     <p>Song Name: <input type="text" name="SongName"/></p>
     <p>Artist: <input type="text" name="Artist"/></p>
